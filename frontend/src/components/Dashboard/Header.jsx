19067e4c228dfc6efba455a4d91c5297164f55c6
Ayush <ayushtomar8624@gmail.com>
2024-10-07 18:47:56 +0530

    Updated package files and Header component

--- frontend/src/components/Dashboard/Header.jsx
+++ frontend/src/components/Dashboard/Header.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useMoralis } from "react-moralis";
+import { ConnectButton } from "web3uikit";
 
 const Header = () => {
-  const { enableWeb3 } = useMoralis();
   const location = useLocation();
 
   if (["/"].includes(location.pathname)) return null;
@@ -12,14 +11,7 @@ const Header = () => {
     <>
       <div className="w-full  flex justify-right text-white bg-black border-gray-700 border-b">
         <div className="flex justify-end p-4 w-full">
-          <div
-            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            onClick={async () => {
-              await enableWeb3();
-            }}
-          >
-            connect
-          </div>
+          <ConnectButton moralisAuth={false} />
           <div>
             <img
               className="w-12 h-12 rounded-full"
