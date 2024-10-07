ec32c3c0f8720a82628a416b1535933370df80c0
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-10-07 18:22:37 +0530

    headers and dash changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { login, signup, sendotp, logout } = require('../controllers/auth');
 const { auth, verifyToken } = require('../middlewares/authMiddle');
-const {Post} = require('../models/post');
-const { setposts, getposts, getUserPosts, deletePost } = require('../controllers/post');
 
 // Public routes
 router.post('/login', login);
@@ -11,11 +9,6 @@ router.post('/signup', signup);
 router.post('/sendotp', sendotp);
 router.get('/verify-token', verifyToken);
 router.get('/logout', logout);
-router.get('/posts', getposts);
-router.post('/posts',auth, setposts);
-router.get('/myposts',auth, getUserPosts);
-router.delete('/posts/:postId', auth, deletePost);
-
 // Testing protected route
 router.get('/test', auth, (req, res) => {
     res.json({
--- frontend/src/components/Dashboard/Header.jsx
+++ frontend/src/components/Dashboard/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useMoralis } from "react-moralis";
+// import { useMoralis } from "react-moralis";
 
 const Header = () => {
-  const { enableWeb3 } = useMoralis();
+//   const { enableWeb3 } = useMoralis();
   const location = useLocation();
 
   if (["/"].includes(location.pathname)) return null;
@@ -15,7 +15,8 @@ const Header = () => {
           <div
             className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             onClick={async () => {
-              await enableWeb3();
+            //   await enableWeb3();
+            console.log("hellojk");
             }}
           >
             connect
